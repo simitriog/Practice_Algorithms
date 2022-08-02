0bd7750626c8c51d270bbfff1235aef0be7802f4
Simitrio Guzman <simitrio.guzman@agilethought.com>
2022-08-02 10:20:09 -0500

    Generate new code

--- App_Test/Program.cs
+++ App_Test/Program.cs
@@ -13,8 +13,8 @@ namespace App_Test
             TheJourneyBegins objJourney = new TheJourneyBegins();
             EdgeOfTheOcean objEdge = new EdgeOfTheOcean();
 
-            int[] inputArray = { 1, 3, 2, 1 };
-            bool res = objEdge.almostIncreasingSequence(inputArray);
+            int[] inputArray = { 10, 1, 2, 3, 4, 5, 6, 1 };
+            var res = objEdge.almostIncreasingSequence(inputArray);
         }
     }
 }
@@ -39,8 +39,6 @@ public class TheJourneyBegins
         int i = 0;
 
         Array.Reverse(arrI);
-
-
 
         while (i < arrI.Count())
         {
@@ -80,20 +78,14 @@ public class EdgeOfTheOcean
     #region Shape Area
 
     public int shapeArea(int n)
-    {
-        int res = 0;
-
+    {       
         if (n == 1)
             return 1;
-        else
-        {
-            res = squareLap(n);
-        }
 
-        return res;
+        return squareLap(n);
     }
 
-    int squareLap(int n)
+    private int squareLap(int n)
     {
         int pA = 0, res = 0;
         for (int i = 1; i < n; i++)
@@ -103,7 +95,8 @@ public class EdgeOfTheOcean
                 pA = (4 * i) + 1;
                 res = pA;
             }
-            else
+
+            if (i != 1)
             {
                 pA = (4 * i) + pA;
                 res = pA;
@@ -151,23 +144,51 @@ public class EdgeOfTheOcean
 
     #endregion
 
+    #region Almost Increasing Sequence
+
     public bool almostIncreasingSequence(int[] sequence)
     {
-        bool res = false;
-        int c = 0;
+
+        if (sequence.Count() == 2)
+            return true;
+
+        bool res = true;
+        int c = 0, d = 0, e = 0;
 
         Array.Sort(sequence);
 
-
-
-        while (c < sequence.Count() - 1)
+        while (c < sequence.Count())
         {
-            int[] tempArr = returnNewArray(sequence);
+            int[] tempArr = returnNewArray(sequence, c);
 
-            //tempArr =  tempArr.Where
+            d = 0;
 
-            if (sequence[c] < tempArr[c + 1])
-                res = true;
+            while (d < tempArr.Count() - 1)
+            {
+                if (tempArr.Count() < 4) if (tempArr[d] == tempArr[d + 1]) return false;
+
+                if (tempArr[d] == tempArr[d + 1])
+                {
+                    if (d == 0 && tempArr[d] == tempArr[d + 1])
+                        return false;
+
+                    int[] tempArr2 = returnNewArray(tempArr, d);
+
+                    e = 0;
+
+                    while (e < tempArr2.Count() - 1)
+                    {
+                        if (tempArr2[e] == tempArr2[e + 1])
+                            return false;
+
+                        e++;
+                    }
+                }
+
+                if (tempArr[d] > tempArr[d + 1]) return false;
+
+                d++;
+            }
 
             c++;
         }
@@ -175,22 +196,263 @@ public class EdgeOfTheOcean
         return res;
     }
 
-    private int[] returnNewArray(int[] array)
+    private int[] returnNewArray(int[] array, int position)
     {
-        int[] res = new int[array.Count()];
-        int c = 0;
+        int[] res = new int[array.Count() - 1];
+        int c = 0, d = 0;
 
         while (c < array.Count())
         {
-            res[c] = array[c];
+            if (c != position)
+                res[d] = array[c]; d++;
+
             c++;
         }
 
         return res;
     }
 
-    private bool removeNumber(int n, int nR)
+    #endregion
+
+    #region Test Solutions
+
+    /*
+     * string[] A = { "sander", "amy", "ann", "michael" };
+       string[] B = { "123456789", "234567890", "789123456", "123123123" };
+       string P = "1";
+     */
+
+    public bool returnValidation(int[] array, int number, int times)
     {
-        return n != nR;
+        int valCount = 0, c = 0;
+
+        Array.Sort(array);
+
+        while (c < array.Count())
+        {
+            if (array[c] == number)
+            {
+                valCount++;
+            }
+
+            c++;
+        }
+
+        if (valCount == times || valCount > times)
+            return true;
+        else
+            return false;
     }
+
+    public int returnDiference(int[] array)
+    {
+        int res = 0, c = 0, d = 0, e = 0;
+
+        Array.Sort(array);
+
+        if (array.Count() <= 3)
+        {
+            res = array[array.Count() - 1] - array[0];
+
+            return res;
+        }
+        else
+        {
+            while (c < array.Count())
+            {
+                if (evenNumber(array[c]) != true)
+                    d++;
+
+                c++;
+            }
+        }
+        int[] objArr = new int[d];
+
+        c = 0;
+        while (c < array.Count())
+        {
+            if (evenNumber(array[c]) != true)
+            {
+                objArr[e] = array[c];
+
+                e++;
+            }
+
+
+            c++;
+        }
+
+        if (objArr.Count() <= 2)
+        {
+            res = objArr[0];
+        }
+        else
+        {
+            res = objArr[objArr.Count() - 1];
+        }
+
+
+        return res;
+    }
+
+    private bool evenNumber(int number)
+    {
+        if ((number % 2) == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public bool returnSameSizeVal(double[] array)
+    {
+        int c = 0, countD = 0;
+        double sizeS = 0;
+        bool res = true;
+
+        Array.Sort(array);
+
+        if (array.Count() < 2)
+            return true;
+
+        while (c < array.Count() - 1)
+        {
+            if (c == 0)
+            {
+                sizeS = Math.Round(array[c + 1] - array[c], 1);
+            }
+            else
+            {
+                if ((Math.Round(array[c + 1] - array[c], 1)) != sizeS)
+                {
+                    if (array[c] == array[c + 1])
+                    {
+                        sizeS = Math.Round(array[c + 1] - array[c], 1);
+                    }
+                    else if (countD < 2)
+                    {
+                        sizeS = Math.Round(array[c + 1] - array[c], 1);
+
+                        countD++;
+                    }
+                    else return false;
+                }
+
+
+            }
+
+            c++;
+        }
+
+        return res;
+    }
+
+    public int returnSmallInteger(int[] array)
+    {
+        int c = 1, d = 0, res = 0;
+        int[] arrOp = new int[array.Count()];
+
+        Array.Sort(array);
+
+        if (array[array.Count() - 1] < 0)
+            return 1;
+        else if (array.Count() < 4 && array[array.Count() - 1] < 4)
+            return 4;
+        else
+        {
+            while (d < array.Count())
+            {
+                arrOp[d] = c;
+
+                c++;
+                d++;
+            }
+
+            c = 0;
+            d = 0;
+
+            while (c < array.Count())
+            {
+                if (array[c] != arrOp[d])
+                {
+                    d++;
+
+                    if (array[c] != arrOp[d])
+                        res = arrOp[d];
+                }
+
+                c++;
+            }
+        }
+
+        return res;
+    }
+
+    public string returnContactName(string[] A, string[] B, string P)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        bool band = false;
+        int c = 0, d = 0, e = 0;
+        string res = string.Empty;
+        string[] objArr = new string[d];
+
+        while (c < B.Count())
+        {
+            if (B[c].Contains(P) && A.Count() <= 2) res = A[c];
+            else if (B[c].Contains(P))
+            {
+                d++;
+                band = true;
+            }
+            else
+                res = "NO CONTACT";
+
+            c++;
+        }
+
+        if (band)
+        {
+            c = 0;
+
+            objArr = new string[d];
+
+            while (c < B.Count())
+            {
+                if (B[c].Contains(P))
+                {
+                    objArr[e] = A[c];
+                    e++;
+                }
+
+                c++;
+            }
+
+            res = A[returnPositionName(A, objArr)];
+        }
+
+
+
+        return res;
+    }
+
+    private int returnPositionName(string[] array, string[] compArr)
+    {
+        int res = 0, c = 0;
+
+        Array.Sort(compArr);
+
+        while (c < array.Count())
+        {
+            if (array[c] == compArr[0])
+                res = c;
+
+            c++;
+        }
+
+
+        return res;
+    }
+
+    #endregion
+
+
 }
